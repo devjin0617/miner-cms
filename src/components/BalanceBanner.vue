034f8bd190c8d9a323ca432e42d440c301cc5fa2
devjin0617 <devjin0617@gmail.com>
2018-03-03 19:02:18 +0900

    코인 잔액 배너 생성

---
+++ src/components/BalanceBanner.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="balance-banner">
+    <div class="balance-banner-box">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'balance-banner'
+}
+</script>
+
+<style lang="scss" scope="scoped">
+.balance-banner {
+  width: 100%;
+  height: 80px;
+  background-color: #F2F6FC;
+  border-radius: 5px;
+  line-height: normal;
+
+  .balance-banner-box {
+    padding: 5px 20px;
+  }
+}
+</style>
