a806bbb215d0aad3c3b3a58a8e0b9a1671c07837
devjin0617 <devjin0617@gmail.com>
2018-03-03 18:04:28 +0900

    sidemenu 추가

--- src/views/SideView.vue
+++ src/views/SideView.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="side-view">
-    SideView
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
   </div>
 </template>
 
@@ -13,5 +47,9 @@ export default {
 <style>
 .side-view {
   height: 100%;
+}
+
+.el-menu {
+  background-color: #D3DCE6;
 }
 </style>
